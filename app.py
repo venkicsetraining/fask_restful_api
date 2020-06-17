6a6420225ef8aa9b3b5af49e022be7456eacce6a
Venkateswarlu Allacheruvu <venkicsetraining@gmail.com>
2020-06-18 00:10:10 +0530

    Fixed erros when running the app first time.

--- app.py
+++ app.py
@@ -14,10 +14,6 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.secret_key = 'gautham'
 api = Api(app)
 
-@app.before_first_request
-def create_tables():
-    db.create_all()
-
 # Change end point /auth to /login 
 # app.config(JWT_AUTH_URL_RULE) = '/login'
 jwt = JWT(app, authenticate, identity)
