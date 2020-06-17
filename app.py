0ea7edc695f5ec4d88bd61a11e72c7c369aae452
Venkateswarlu Allacheruvu <venkicsetraining@gmail.com>
2020-06-18 00:36:32 +0530

    Edited the app.py and run.py files

--- app.py
+++ app.py
@@ -27,6 +27,3 @@ api.add_resource(UserRegister, '/register')
 api.add_resource(Store,'/store/<string:name>')
 api.add_resource(StoreList,'/stores')
 
-if __name__ == "__main__":
-    db.init_app(app)
-    app.run(port=5000, debug=True)
--- run.py
+++ run.py
@@ -1,4 +1,4 @@
-from app import app
+from app import app as application
 from db import db
 
 db.init_app(app)
@@ -6,3 +6,6 @@ db.init_app(app)
 @app.before_first_request
 def create_tables():
     db.create_all()
+
+if __name__ == "__main__":
+    app.run(port=5000, debug=True)
