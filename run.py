b3c52658a377f5e0a68ee840be0b97d090bdf49a
Venkateswarlu Allacheruvu <venkicsetraining@gmail.com>
2020-06-18 01:22:49 +0530

    edited in run.py

--- run.py
+++ run.py
@@ -1,11 +1,11 @@
 from app import app as application
 from db import db
 
-db.init_app(app)
+db.init_app(application)
 
 @app.before_first_request
 def create_tables():
     db.create_all()
 
 if __name__ == "__main__":
-    app.run()
+    application.run()
